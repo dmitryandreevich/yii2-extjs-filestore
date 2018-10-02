c66adf0eb88c55f68b516d24217e6b02376f259e
Dmitry Skripak <zverdiman9875@gmail.com>
2018-10-02 16:35:40 +0500

    [New] Added: upload files to store. Added main extjs application.

--- views/main/index.php
+++ views/main/index.php
@@ -1,2 +1,2 @@
 
-<iframe src="http://t-maxim/web/index.html" frameborder="0" width="500px" height="500px"></iframe>
+<iframe src="http://t-maxim/web/fileStoreApp/index.html" frameborder="0" id="application"></iframe>
